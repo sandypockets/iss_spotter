90c134e1a75fe70e8aef8718eedb5855910c687d
Zac Alexander <zac@znacreative.com>
2021-03-12 02:59:35 +0000

    Formatted output of ISS passtimes

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const { nextISSTimesForMyLocation } = require('./iss');
 
+const convertPassTimes = (passTimes) => {
+  console.log('The ISS will be overhead your location at the following times:')
+  for (let time of passTimes) {
+    let dateTime = new Date(0);
+    dateTime.setUTCSeconds(time.risetime);
+    let duration = time.duration;
+    let durationInMins = duration / 60;
+    durationInMins = Math.round(durationInMins);
+    console.log(`${dateTime} for ${durationInMins} minutes.`);
+  }
+};
 
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("It didn't work!", error);
   }
-  console.log(passTimes);
+  convertPassTimes(passTimes);
 });
