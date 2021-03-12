5bafd15298932cbff884b2cd5b2885458f6adcae
Zac Alexander <zac@znacreative.com>
2021-03-12 02:32:24 +0000

    Cleaned up unnecessary exports/comments

--- iss.js
+++ iss.js
@@ -74,4 +74,4 @@ const nextISSTimesForMyLocation = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation };
+module.exports = { nextISSTimesForMyLocation };
